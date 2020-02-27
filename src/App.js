fd5903406bd635d8a14c62894cffeeb22f5f5925
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-28 03:24:41 +0400

    text+ refactoring
    text+ refactoring

--- src/App.js
+++ src/App.js
@@ -68,7 +68,8 @@ class App extends React.Component {
 			</div>
 		);
 	}
-}
 
-export default App;
+	export
+	default
+	App;
 
--- src/TodoListTask.js
+++ src/TodoListTask.js
@@ -1,22 +1,21 @@
 import React from 'react';
 
 class TodoListTask extends React.Component {
-    onIsDoneChanges = (e) => {
-        this.props.changeStatus(this.props.task, e.currentTarget.checked)
-    };
+	onIsDoneChanges = (e) => {
+		this.props.changeStatus(this.props.task, e.currentTarget.checked)
+	};
 
-
-    render = () => {
-        return (
-            <div className="todoList-task">
-                <input type="checkbox"
-                       checked={this.props.task.isDone}
-                       onChange={this.onIsDoneChanges}
-                />
-                <span>{this.props.task.title}, priority:{this.props.task.priority}</span>
-            </div>
-         );
-    }
+	render = () => {
+		return (
+			<div className="todoList-task">
+				<input type="checkbox"
+					   checked={this.props.task.isDone}
+					   onChange={this.onIsDoneChanges}
+				/>
+				<span>{this.props.task.title}, priority:{this.props.task.priority}</span>
+			</div>
+		);
+	}
 }
 
 export default TodoListTask;
